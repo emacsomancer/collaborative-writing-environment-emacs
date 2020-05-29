fe028e6e8f42c4ae11516e3ff2a22ef54638dc3b
Benjamin Slade <slade@jnanam.net>
2020-05-28 19:10:36 -0600

    fix launcher

--- launcher.sh
+++ launcher.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-ssh -Y USER@192.168.1.LOCAL_IP "/PATH/TO/emacsclient -c -a '(treemacs)' &
+ssh -Y USER@192.168.1.LOCAL_IP "emacsclient -c -a '' -e '(treemacs)' &"
+
+# change USER to the relevant user on the base machine
+# change LOCAL_IP (or maybe the entire address) to the relevant IP
 
